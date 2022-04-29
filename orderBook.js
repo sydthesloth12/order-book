035c9fc4a6ccc7106dd1d8d2c277b8cd471d1f60
Sydney <sydneysirois17@gmail.com>
2022-04-28 21:19:06 -0400

    finished

--- orderBook.js
+++ orderBook.js
@@ -1,6 +1,6 @@
 const reconcileOrder (array, object) => {
     let existingBook = [...array]
-    let incomingOrder = [...object]
+    let currentOrder = [...object]
 
     let procuredArray = []
     let nonProcuredArray = [] 
@@ -11,9 +11,43 @@ const reconcileOrder (array, object) => {
         return nonProcuredArray
     }
 
-    
+    existingBook.forEach(existingOrder =>{
+        if(reqTypesMatch(existingOrder, currentOrder) && reqPriceMet (existingOrder, currentOrder)){
+            let dealQuantity = math.min(existingOrder, currentOrder)
+
+            existingOrder.quantity= existingOrder.quantity - dealQuantity 
+
+            currentOrder.quantity= currentOrder.quantity - dealQuantity
+            
+            if(existingOrder.quantity > 0){
+                procuredArray.push(existingOrder)
+            } 
+
+        } else {
+           nonProcuredArray.push(existingOrder) 
+        } 
+    }) 
+
+    if(currentOrder.quantity > 0){
+        procuredArray.push(currentOrder)
+    }
+
 
     return [...nonProcuredArray, ...procuredArray]
+
+}
+
+const reqTypesMatch = (existingOrder, currentOrder) => {
+    return existingOrder.type !== currentOrder.type
+}
+
+const reqPriceMet = (existingOrder, currentOrder) => {
+    if(existingOrder.type === 'sell' && existingOrder.price <= currentOrder.price){
+        return true
+    } if (currentOrder.type === 'sell' && currentOrder.price <= existingOrder.price){
+        return true
+    } return false 
+  
 
 }
 
